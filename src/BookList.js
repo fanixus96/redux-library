993c284825e47435b7b602c9102bcbcaf3e18a2a
Michał Siwek <mike21@aol.pl>
2019-01-29 20:57:34 +0100

    Fix indendation, fix scope access

--- src/BookList.js
+++ src/BookList.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 
 class BookList extends Component {
 
-	componentDidMount() {
-		console.log(this.props.books)
-	}
+  componentDidMount() {
+    console.log(this.props.books)
+  }
 
-    new() {
-        var author = document.getElementById("newAuthor").value;
-        var title = document.getElementById("newTitle").value;
-        this.props.addBook(author, title)
-        console.log(this.props.books)
-    }
+  new() {
+      var author = document.getElementById("newAuthor").value;
+      var title = document.getElementById("newTitle").value;
+      this.props.addBook(author, title)
+      console.log(this.props.books)
+  }
 
-    borrow(item) {
-    	var index = this.props.books.indexOf(item)
-    	console.log(item)
-    	this.props.borrowBook(item)
-    	
-    }
+  borrow(e, item) {
+    console.log(item);
+    this.props.borrowBook(item);
+
+    e.preventDefault();
+  }
 
 
-	render() {
-        const listItems = this.props.books.map((d) => <li onClick={this.borrow.bind(this,d.id)} key={d.id}>{d.author} {d.title} {d.taken}</li>)
+  render() {
+        const listItems = this.props.books.map((d) => <li onClick={(e) => this.borrow(e, d)} key={d.id}>{d.author} {d.title} {d.taken}</li>)
         return (
-        	<div>
-        	{listItems}
+          <div>
+          {listItems}
             <input id="newAuthor"/>
             <input id="newTitle"/>
             <button onClick={this.new.bind(this)} type="submit" id="add">add</button>
-        	</div>
+          </div>
         );
   }
 }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 let nextBookId = 2
 export function addBook(author, title) {
-	return {
-		type: 'ADD_BOOK',
-		id: nextBookId++,
-		author,
-		title
-	}
+  return {
+    type: 'ADD_BOOK',
+    id: nextBookId++,
+    author,
+    title
+  }
 }
 
 export function borrowBook(id) {
-	return {
-		type: 'BORROW_BOOK',
-		id
-	}
+  return {
+    type: 'BORROW_BOOK',
+    id: id,
+  }
 }
